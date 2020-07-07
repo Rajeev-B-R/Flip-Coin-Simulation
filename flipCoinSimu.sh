7dbe18b384c12283c9a8e4b93054d281cdb98892
Rajeev <rajeevsuchiha@gmail.com>
2020-07-07 15:23:29 +0530

    Added: Code to flip coin multiple times and display no of head/tail added

--- flipCoinSimu.sh
+++ flipCoinSimu.sh
@@ -2,12 +2,19 @@ echo "**********Welcome to Flip Coin Simulation**********"
 
 #!/bin/bash -x
 
-flip=$(($(($RANDOM%10))%2))
+heads=0;
+tails=0;
 
-if [ $flip -eq 1 ]
-then
-echo "Heads!"
-else
-echo "Tails!"
-fi
+for (( noTrails=1; noTrails<=20; noTrails++ ))
+do	
+        number=$((RANDOM%2))
+        if [ $number -eq 1 ]
+        then
+                (( heads ++ ))
+                echo "Head count: $heads"
+        else
+                (( tails ++ ))
+                echo "Tail count: $tails"
+        fi
+done
 
